167e840a4212e44eab63047191d4699d4f65968f
Taras Kosyi <work.taras.kosyi@gmail.com>
2025-01-21 18:35:54 +0200

    init

---
+++ ec-2/control-node.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo yum install git -y
+git clone https://github.com/pi88a/ansible-sandbox.git
+
+# Configure AWS CLI
+aws_cli_profile_name=ansible
+aws_access_key_id=$1
+aws_secret_access_key=$2
+
+mkdir -p ~/.aws
+echo -e "[$aws_cli_profile_name]\naws_access_key_id=$aws_access_key_id\naws_secret_access_key=$aws_secret_access_key" > ~/.aws/credentials
+echo -e "[$aws_cli_profile_name]\nregion=eu-central-1" > ~/.aws/config
+export AWS_PROFILE=$aws_cli_profile_name
+
+# Install pip
+curl -O https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py --user
+echo -e 'export PATH=/home/ec2-user/.local/bin:$PATH'  >> ~/.bash_profile
+source ~/.bash_profile
+
+# Install Ansible
+python3 -m pip install --user ansible
+pip install boto3
